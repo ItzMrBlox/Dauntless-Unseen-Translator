7a2ba3102ffa211633cb576794426d0369c74854
Blox <48685514+ItzMrBlox@users.noreply.github.com>
2020-07-01 12:31:23 +0200

    Delete misc.js

--- scripts/misc.js
+++
@@ -1,7 +0,0 @@
-/* document.getElementById("home").addEventListener("click", function(){
-    window.location.assign(".index.html");
-});
-
-document.getElementById("translatorbutton").addEventListener("click", function(){
-    window.location.assign(".Translation.html");
-}); */
