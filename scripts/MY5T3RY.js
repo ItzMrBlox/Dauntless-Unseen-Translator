29b7f188260d7f871a0fb3fa6356382ca90503df
Blox <48685514+ItzMrBlox@users.noreply.github.com>
2020-07-05 02:51:12 +0200

    Added support for mobile and included attribute

--- scripts/MY5T3RY.js
+++
@@ -1,7 +0,0 @@
-function MouseOver() {
-    document.getElementById("myImg").src = "../assets/images/Dauntless-logo.gif";
-}
-
-function MouseOut() {
-    document.getElementById("myImg").src = "../assets/images/Dauntless Logo.png";
-}
