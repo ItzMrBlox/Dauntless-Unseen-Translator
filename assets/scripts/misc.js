5529b90eefe9688c1a048b67bb0d45cac89195ff
Blox <48685514+ItzMrBlox@users.noreply.github.com>
2020-10-18 17:23:27 +0200

    Update misc.js

--- assets/scripts/misc.js
+++ assets/scripts/misc.js
@@ -1,37 +1,36 @@
 function includeHTML() {
-    var z, i, elmnt, file, xhttp;
-    /*loop through a collection of all HTML elements:*/
-    z = document.getElementsByTagName("*");
-    for (i = 0; i < z.length; i++) {
-      elmnt = z[i];
-      /*search for elements with a certain atrribute:*/
-      file = elmnt.getAttribute("include-html");
-      if (file) {
-        /*make an HTTP request using the attribute value as the file name:*/
-        xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-          if (this.readyState == 4) {
-            if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-            if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-            /*remove the attribute, and call this function once more:*/
-            elmnt.removeAttribute("include-html");
-            includeHTML();
-          }
-        }      
-        xhttp.open("GET", file, true);
-        xhttp.send();
-        /*exit the function:*/
-        return;
+  var z, i, elmnt, file, xhttp;
+  /*loop through a collection of all HTML elements:*/
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("include-html");
+    if (file) {
+      /*make an HTTP request using the attribute value as the file name:*/
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function () {
+        if (this.readyState == 4) {
+          if (this.status == 200) { elmnt.innerHTML = this.responseText; }
+          if (this.status == 404) { elmnt.innerHTML = "Page not found."; }
+          /*remove the attribute, and call this function once more:*/
+          elmnt.removeAttribute("include-html");
+          includeHTML();
+        }
       }
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /*exit the function:*/
+      return;
     }
+  }
 };
 
 
 //sa421343r54235 23b523  523 g5235e<btrewteT 345t345 
-function MouseOver() {
-    document.getElementById("myImg").src = "../assets/images/Dauntless-logo.gif";
-}
-
-function MouseOut() {
-    document.getElementById("myImg").src = "../assets/images/Dauntless Logo.png";
-}
+document.getElementById("logo").addEventListener('mouseover', function () {
+  document.getElementById("logo").src = "../assets/images/Dauntless-logo.gif";
+  this.addEventListener('mouseout', function(){
+    document.getElementById("logo").src = "../assets/images/Dauntless Logo.png";
+  })
+});
